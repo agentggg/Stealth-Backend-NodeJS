d7f78dad2a258cd5b4d33bbb3e4a74a8f4836f31
Your Name <your-email@example.com>
2024-10-31 16:40:04 -0400

    automated upload

--- controllers/recordManagement.js
+++ controllers/recordManagement.js
@@ -6,7 +6,7 @@ const moment = require('moment')
 
 exports.add = async (req, res) => {
     try{
-        const { workout_id, username, sets, reps, time, total_rest_time } = req.body;
+        const { workout_id, username, sets, reps, time, total_rest_time, weight } = req.body;
         const username_lookup = await User.findOne({ username: username });
         const exercise_lookup = await Exercise.findOne({ _id: workout_id });
         const today = moment();
@@ -20,6 +20,7 @@ exports.add = async (req, res) => {
             time:time,
             total_rest_time:total_rest_time,
             day_of_week:day_of_week,
+            weight:weight
         })
         res.status(200).json({
             status:'success',
--- models/Records.js
+++ models/Records.js
@@ -9,6 +9,7 @@ const RecordsSchema = new mongoose.Schema({
     workouts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Exercise' }], // Reference to Exercise model
     sets: { type: Number, default: 0 },
     reps: { type: Number, default: 0 },
+    weight: { type: Number, default: 0 },
     time: { type: String, default: "05:21" },
     total_rest_time: { type: String, default: "17:32" },
     day_of_week: { type: String },
