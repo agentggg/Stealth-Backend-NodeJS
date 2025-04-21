c102b87df8185152df8ff425f3072d5c6e88a0dc
Your Name <your-email@example.com>
2025-04-21 16:49:26 -0400

    automated upload

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/index', indexRouter)
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 app.use('/workout', workoutManagementRouter)
